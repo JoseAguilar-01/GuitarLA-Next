c7cd0707a25ac7a24d226d4594432f294a4345d3
Jose Aguilar <jose.aguilar.developer@gmail.com>
2024-01-20 10:33:59 -0400

    Modification: formatted code

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import Link from 'next/link.js';
+import styles from '../styles/NoEncontrado.module.css';
+
+const NoEncotrado = () => {
+  return (
+    <div className={styles.no_encontrado}>
+      <h1 className='heading'>Página no encotrada</h1>
+
+      <Link href='/'> Volver a Inicio</Link>
+    </div>
+  );
+};
+
+export default NoEncotrado;
---
+++ src/pages/nosotros.js
@@ -0,0 +1,46 @@
+import Image from 'next/image';
+import Layout from '../components/Layout';
+import styles from '../styles/Nosotros.module.css';
+
+const Nosotros = () => {
+  return (
+    <Layout pagina={'Nosotros'}>
+      <main className='contenedor'>
+        <h2 className='heading'>Nosotros</h2>
+
+        <div className={styles.contenido}>
+          <Image
+            layout='responsive'
+            src='/img/nosotros.jpg'
+            alt='nosotros image'
+            width={600}
+            height={450}
+          />
+
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto eum
+              voluptatum mollitia ratione doloribus in sapiente est facilis, ex,
+              aliquam necessitatibus! Aut nulla quaerat vero aspernatur. Atque
+              animi ex expedita labore voluptatem neque obcaecati. Nemo est
+              necessitatibus excepturi, ipsam illo hic ex odit mollitia harum
+              sapiente quos tempora voluptatibus optio corrupti eaque blanditiis
+              deleniti, consequuntur esse, possimus nam porro impedit eos.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto eum
+              voluptatum mollitia ratione doloribus in sapiente est facilis, ex,
+              aliquam necessitatibus! Aut nulla quaerat vero aspernatur. Atque
+              animi ex expedita labore voluptatem neque obcaecati. Nemo est
+              necessitatibus excepturi, ipsam illo hic ex odit mollitia harum
+              sapiente quos tempora voluptatibus optio corrupti eaque blanditiis
+              deleniti, consequuntur esse, possimus nam porro impedit eos.
+            </p>
+          </div>
+        </div>
+      </main>
+    </Layout>
+  );
+};
+
+export default Nosotros;
