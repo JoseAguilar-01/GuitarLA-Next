5a497470e29f393a6a9ac71e702c4d820e51feef
Jose Aguilar <jose.aguilar.developer@gmail.com>
2024-01-22 11:41:55 -0400

    Modificatio: add missing components imports to the index.ts file of the components folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { Footer } from './Footer';
 export { GuitarList } from './GuitarList';
 export { Header } from './Header';
+export { Course } from './Course';
+export { EntriesList } from './EntriesList';
